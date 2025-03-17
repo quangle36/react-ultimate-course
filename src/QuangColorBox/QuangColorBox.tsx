d7d48186511280fe33e1af6b1395eeb731443305
Quang Le <quanglm>
2025-03-17 13:34:57 +0700

    create color box

--- src/QuangColorBox/QuangColorBox.tsx
+++ src/QuangColorBox/QuangColorBox.tsx
@@ -1,60 +1,68 @@
-import { useState } from "react";
-import styles from "./QuangColorBox.module.css";
+import { useState } from 'react';
+import styles from './QuangColorBox.module.css';
+import clsx from 'clsx';
+import React from 'react';
 enum Colors {
-  AQUA = "AQUA",
-  RED = "RED",
-  YELLOW = "YELLOW",
-  PURPLE = "PURPLE",
+	AQUA = 'AQUA',
+	RED = 'RED',
+	YELLOW = 'YELLOW',
+	PURPLE = 'PURPLE',
+	RANDOM = 'RANDOM',
+	ANTIQUEWHITE = 'ANTIQUEWHITE',
 }
 const QuangColorBox = () => {
-  const [activeColor, setActiveColor] = useState("default");
+	const [activeColor, setActiveColor] = useState<Colors>(Colors.ANTIQUEWHITE);
+	const [boxes, setBoxes] = useState(0);
+	const [isGenerated, setIsGenerated] = useState(false);
+	const [, updateState] = React.useState();
+	const forceUpdate = React.useCallback(() => updateState({}), []);
 
-  const boxes = [
-    {
-      color: Colors.AQUA,
-      label: "Aqua",
-    },
-    {
-      color: Colors.RED,
-      label: "Red",
-    },
-    {
-      color: Colors.YELLOW,
-      label: "Yellow",
-    },
-    {
-      color: Colors.PURPLE,
-      label: "Purple",
-    },
-  ];
-
-  const handleOnClickBox = (color: Colors) => {
-    console.log("color", color);
-    if (color === activeColor) {
-      setActiveColor("default");
-    } else {
-      setActiveColor(color);
-    }
-  };
-  return (
-    <>
-      <div>Current color {activeColor}</div>
-      <div className={styles.wrapper}>
-        {boxes.map((box) => (
-          <div
-            style={{
-              backgroundColor:
-                activeColor === "default" ? box.color : activeColor,
-            }}
-            onClick={() => handleOnClickBox(box.color)}
-            className={styles["box-container"]}
-          >
-            {box.label}
-          </div>
-        ))}
-      </div>
-    </>
-  );
+	const handleSetColor = (color: Colors) => {
+		setActiveColor(color);
+	};
+	const genRandomColor = () => {
+		return '#' + Math.floor(Math.random() * 16777215).toString(16);
+	};
+	return (
+		<>
+			<div>
+				Number of boxes{' '}
+				<input
+					type="number"
+					min={1}
+					max={128}
+					onChange={(e) => {
+						setActiveColor(Colors.ANTIQUEWHITE);
+						setIsGenerated(false);
+						setBoxes(+e.target.value);
+					}}
+				/>
+				<button onClick={() => setIsGenerated(true)}>Generate</button>
+			</div>
+			<div className={styles.wrapper}>
+				{boxes === 0 ? (
+					<div>No box</div>
+				) : (
+					isGenerated &&
+					Array.from({ length: boxes }).map((genBox, index) => (
+						<div
+							style={{
+								backgroundColor:
+									(activeColor === Colors.RANDOM && genRandomColor()) || '',
+							}}
+							onClick={() => {
+								forceUpdate();
+								handleSetColor(Colors.RANDOM);
+							}}
+							className={clsx(styles['box-container'])}
+						>
+							Box #{index + 1}
+						</div>
+					))
+				)}
+			</div>
+		</>
+	);
 };
 
 export default QuangColorBox;
